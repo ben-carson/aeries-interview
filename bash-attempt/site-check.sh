88be59d39e77948c9640b1412f817352708d5a9b
Ben Carson <git@bencarson.net>
2022-04-27 22:58:34 -0400

    little closer to a prototype

--- bash-attempt/site-check.sh
+++ bash-attempt/site-check.sh
@@ -1,5 +1,5 @@
 # be sure you set this file to be executable
-wget -i ./URLsToCheck.txt
+wget -S -w 5 -i ./URLsToCheck.txt -o ./CheckResults.log
 
 #TODO: parse the first index.html to ensure it exists
 #TODO: parse the 2nd index.html.1 to ensure it exists
